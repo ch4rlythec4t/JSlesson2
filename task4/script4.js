f87f96f2747d39947725bc480b6b39b163992778
Sergey Vorobev <ch4rlythec4t@gmail.com>
2023-01-28 19:49:01 +0300

    Task4 done

---
+++ task4/script4.js
@@ -0,0 +1,10 @@
+const num = prompt("Введите число")
+if (num < 5)
+{
+    alert("Число меньше 5")
+}
+    else if (num > 5)
+    {
+        alert("Число больше 5")  
+    }
+    else  alert("Число равно 5")  
