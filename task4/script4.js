37c4b9a8120af7b04022420273283880c7f1f39e
Sergey Vorobev <ch4rlythec4t@gmail.com>
2023-01-28 19:59:39 +0300

    Task4 modiff

--- task4/script4.js
+++ task4/script4.js
@@ -8,3 +8,17 @@ if (num < 5)
         alert("Число больше 5")  
     }
     else  alert("Число равно 5")  
+
+    // const test1 = 5;
+    // const test2 = 5;
+    // let result;
+    // result = (test1 === test2) ? "Равны" : "Не равны";
+    // console.log(result);
+
+    // const test1 = +prompt("Введите первое число");
+    // const test2 = +prompt("Введите второе число");
+    // let result;
+    // result = (test1 > test2) ? `${test1} больше ${test2}` : `${test1} меньше ${test2}` ;
+    // console.log(result);
+
+
